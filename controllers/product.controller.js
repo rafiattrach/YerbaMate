27515dfac359aefc1c3ed080cc7c31763a309a4c
Rafi Attrach <rafialattrach@gmail.com>
2020-05-28 06:00:41 +0200

    new route and controller for product details setup

---
+++ controllers/product.controller.js
@@ -0,0 +1,12 @@
+exports.getProduct = (req, res, next) => {
+
+    // search for id 
+
+    let id = req.params.id
+
+    // get the product from DB (in model)
+
+
+    // render ejs file
+
+}
---
+++ routes/product.route.js
@@ -0,0 +1,8 @@
+// work on router level
+const router = require("express").Router()
+
+// parameters after : 
+router.get("/product/:id")
+
+
+module.exports = router
